3fcbd7d0f37c8277d05d834d40522083836b2b2e
LAUNAY Orian Merry Timéo <186729939+HomeyMura@users.noreply.github.com>
2024-10-29 16:54:20 +0100

    co

--- pil/pil.py
+++
@@ -1,6 +0,0 @@
-from PIL import ImageFont, Image, ImageDraw
-
-
-im = Image.open(f"{os.path.dirname(os.path.abspath(__file__))}/activity chart.jpg")
-
-im.save(f"{os.path.dirname(os.path.abspath(__file__))}/output.png", "PNG")
