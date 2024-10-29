f8fed3fe082db331388c641db36e2d3646e58a87
Your Name <you@example.com>
2024-10-29 21:12:27 +0100

    pas de nb_troupe de départ

--- classes.py
+++ classes.py
@@ -2,15 +2,11 @@ class Configuration:
     vaisseaux_at : list[int, int, int] # [nb_vaisseaux_capitaux, nb_bombardiers, nb_chasseurs] pour l'attaquant
     vaisseaux_de : list[int, int, int] # [nb_vaisseaux_capitaux, nb_bombardiers, nb_chasseurs] pour le défenseur
     base : bool # True si base impériale présente
-    nb_troupes_at : int
-    nb_troupes_de : int
 
-    def __init__(self, vaisseaux_at, vaisseaux_de, base, nb_troupes_at, nb_troupes_de):
+    def __init__(self, vaisseaux_at, vaisseaux_de, base):
         self.vaisseaux_at = vaisseaux_at
         self.vaisseaux_de = vaisseaux_de
         self.base = base
-        self.nb_troupes_at = nb_troupes_at
-        self.nb_troupes_de = nb_troupes_de
 
 
 class case:
