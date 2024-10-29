8a713b452b464c4131d0de0d4ab8b696e02a2e60
HomeyMura <186729939+HomeyMura@users.noreply.github.com>
2024-10-29 17:53:04 +0100

    c

--- pilo/pilo.py
+++ pilo/pilo.py
@@ -1,3 +1,4 @@
+import os
 from PIL import ImageFont, Image, ImageDraw
 
 
@@ -11,11 +12,18 @@ vaisseaux?
 
 dirName= os.path.dirname(os.path.abspath(__file__))
 im = Image.new("RGB", (2000, 2000), "white")
-title_font = ImageFont.truetype(f"{dirName}/Marianne/Marianne-ExtraBold.otf",
+titleFont = ImageFont.truetype(f"{dirName}/Marianne/Marianne-ExtraBold.otf",
 size=30)
-body_font = ImageFont.truetype(f"{dirName}/Marianne/Marianne-Regular.otf", size=20)
-
+generalFont = ImageFont.truetype(f"{dirName}/Marianne/Marianne-Regular.otf", size=20)
 draw = ImageDraw.Draw(im)
 
+def drawTable():
+    xbegin = 50
+    ybegin = 50
+    for i in range(10):
+        for j in range(10):
+            draw.text((xbegin+i*50, ybegin+j*50), "Hello", font=generalFont, fill=(0, 0, 0))
+
+drawTable()
 
 im.save(f"{dirName}/output.png", "PNG")
