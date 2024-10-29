dfc6274077b3cce08ad92d8b56dd8fa9e2d1263e
HomeyMura <186729939+HomeyMura@users.noreply.github.com>
2024-10-30 00:44:22 +0100

    f

--- main.py
+++ main.py
@@ -36,9 +36,3 @@ if __name__=="__main__":
 
 
     
-
-
-# configuration = Configuration([1, 0, 3],[0, 0, 0], False)
-# risk_prob = RiskProbaCalculator(configuration)
-# print(risk_prob.compute_all(10, 10))
-
