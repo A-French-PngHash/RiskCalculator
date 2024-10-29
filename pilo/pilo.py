74c6e5008e78faf48dd07e7e54311cb435c43a39
HomeyMura <186729939+HomeyMura@users.noreply.github.com>
2024-10-29 21:03:00 +0100

    re

--- pilo/pilo.py
+++ pilo/pilo.py
@@ -1,9 +1,10 @@
 import os
 from PIL import ImageFont, Image, ImageDraw
+import sys
+dirName= os.path.dirname(os.path.abspath(__file__))
+sys.path.append(f'{dirName}/../')
 from classes import tableau, Configuration
 
-
-dirName= os.path.dirname(os.path.abspath(__file__))
 config = Configuration([2,3,3],[2,3,3],True,50,50)
 tab = tableau(config,[["qejkfdgs","dsfds"],["dsfds","sfsf"]])
 im = Image.new("RGB", (1000*len(tab.liste_proba), 1000*len(tab.liste_proba)), "white")
@@ -12,14 +13,14 @@ size=30)
 generalFont = ImageFont.truetype(f"{dirName}/Marianne/Marianne-Regular.otf", size=20)
 draw = ImageDraw.Draw(im)
 
-draw.text((100, 100), f"Nombre de vaisseaux capitaux de l'attaquant : {str(config.vaisseaux_j1[0])}", font=generalFont, fill=(0, 0, 0))
-draw.text((100, 150), f"Nombre de vaisseaux capitaux du défenseur : {str(config.vaisseaux_j2[0])}", font=generalFont, fill=(0, 0, 0))
-draw.text((100, 200), f"Nombre de chasseurs de l'attaquant : {str(config.vaisseaux_j1[2])}", font=generalFont, fill=(0, 0, 0))
-draw.text((100, 250), f"Nombre de chasseurs du défenseur : {str(config.vaisseaux_j2[2])}", font=generalFont, fill=(0, 0, 0))
-draw.text((100, 300), f"Nombre de bombardiers de l'attaquant : {str(config.vaisseaux_j1[1])}", font=generalFont, fill=(0, 0, 0))
-draw.text((100, 350), f"Nombre de bombardiers du défenseur : {str(config.vaisseaux_j2[1])}", font=generalFont, fill=(0, 0, 0))
-draw.text((100, 400), f"Nombre de troupes de l'attaquant : {str(config.nb_troupes_j1)}", font=generalFont, fill=(0, 0, 0))
-draw.text((100, 450), f"Nombre de troupes du défenseur : {str(config.nb_troupes_j2)}", font=generalFont, fill=(0, 0, 0))
+draw.text((100, 100), f"Nombre de vaisseaux capitaux de l'attaquant : {str(config.vaisseaux_at[0])}", font=generalFont, fill=(0, 0, 0))
+draw.text((100, 150), f"Nombre de vaisseaux capitaux du défenseur : {str(config.vaisseaux_de[0])}", font=generalFont, fill=(0, 0, 0))
+draw.text((100, 200), f"Nombre de chasseurs de l'attaquant : {str(config.vaisseaux_at[2])}", font=generalFont, fill=(0, 0, 0))
+draw.text((100, 250), f"Nombre de chasseurs du défenseur : {str(config.vaisseaux_de[2])}", font=generalFont, fill=(0, 0, 0))
+draw.text((100, 300), f"Nombre de bombardiers de l'attaquant : {str(config.vaisseaux_at[1])}", font=generalFont, fill=(0, 0, 0))
+draw.text((100, 350), f"Nombre de bombardiers du défenseur : {str(config.vaisseaux_de[1])}", font=generalFont, fill=(0, 0, 0))
+draw.text((100, 400), f"Nombre de troupes de l'attaquant : {str(config.nb_troupes_at)}", font=generalFont, fill=(0, 0, 0))
+draw.text((100, 450), f"Nombre de troupes du défenseur : {str(config.nb_troupes_de)}", font=generalFont, fill=(0, 0, 0))
 draw.text((100, 500), f"Y a-t-il une base ? : {str(config.base)}", font=generalFont, fill=(0, 0, 0))
 
 
@@ -31,15 +32,15 @@ def drawTable(valueList):
     margeInterieure = 10
 
     #Dessine les fonds
-    for i in range(len(valueList)):
-        for j in range(len(valueList[0])):
+    for i,liste in enumerate(valueList):
+        for j,case in enumerate(valueList[0]):
             x0 = tablexpos + j * largeurCellule
             y0 = tableypos + i * hauteurCellule
             x1 = x0 + largeurCellule
             y1 = y0 + hauteurCellule
-            if 1>0.5:
+            if case.proba >0.5:
                 couleur = (0,255,0)
-            elif 1<0.5:
+            elif case.proba <0.5:
                 couleur = (255,0,0)
             else:
                 couleur = (255,165,0)
