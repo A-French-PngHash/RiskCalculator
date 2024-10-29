6064804a8176204e0b713214d86c4a91ca1001b8
Titouan <75045140+PngHash-Titouan@users.noreply.github.com>
2024-10-29 22:34:03 +0100

    Computation is now working and a interface is provided

---
+++ classes.py
@@ -0,0 +1,39 @@
+class Configuration:
+    vaisseaux_att : list[int, int, int] # [nb_vaisseaux_capitaux, nb_bombardiers, nb_chasseurs] pour le joueur 1
+    vaisseaux_def : list[int, int, int] # [nb_vaisseaux_capitaux, nb_bombardiers, nb_chasseurs] pour le joueur 2
+    base : bool # True si base impériale présente
+
+    def __init__(self, vaisseaux_att, vaisseaux_def, base):
+        self.vaisseaux_att = vaisseaux_att
+        self.vaisseaux_def = vaisseaux_def
+        self.base = base
+
+    def __repr__(self) -> str:
+        return str(self.vaisseaux_att) + " " + str(self.vaisseaux_def) + " Base : " + ("oui" if self.base else "non")
+
+
+class Case:
+    proba : float # probabilité que le joueur 1 gagne
+    nb_troupes_restantes : int # espérance des troupes restantes du joueur 1
+
+    def __init__(self, proba, nb_troupes_restantes):
+        self.proba = proba
+        self.nb_troupes_restantes = nb_troupes_restantes
+
+    def __str__(self) -> str:
+        return str(self.proba)
+    
+    def __repr__(self) -> str:
+        return str(self.proba)
+
+
+class Tableau:
+    config : Configuration
+    liste_proba : list[list[Case]]
+
+    def __init__(self, config, liste_proba):
+        self.config = config
+        self.liste_proba = liste_proba
+
+    def __repr__(self):
+        return "Configuration : " + str(self.config) + "\n" + "Probabilités : " + str(self.liste_proba)
