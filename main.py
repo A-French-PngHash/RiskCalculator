c587d005b2f06964199546c9839457df462ff0e2
Titouan <75045140+PngHash-Titouan@users.noreply.github.com>
2024-10-30 00:07:21 +0100

    make image drawing service

--- main.py
+++ main.py
@@ -7,3 +7,5 @@ from proba_calc.proba_calc import RiskProbaCalculator
 configuration = Configuration([1, 0, 3],[0, 0, 0], False)
 risk_prob = RiskProbaCalculator(configuration)
 print(risk_prob.compute_all(10, 10))
+
+
