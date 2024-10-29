0acacac06c85e445c354c33d2e2c340be645d1b2
Titouan <75045140+PngHash-Titouan@users.noreply.github.com>
2024-10-30 00:50:39 +0100

    add mkdir

--- main.py
+++ main.py
@@ -29,11 +29,13 @@ def config():
 
 if __name__=="__main__":
     #configuration = config()
-    configuration = Configuration([1, 0, 0], [0, 0, 0], False)
+    configuration = Configuration([2, 2, 2], [0, 0, 0], False)
     risk_prob = RiskProbaCalculator(configuration)
     tab = risk_prob.compute_all(5, 5)
     image = ImageDrawingService()
     dir = f"{dirName}/output"
+
+    os.mkdir(dir)
     numbers = [int(i[:-4]) for i in os.listdir(dir) if i[:-4].isnumeric()]
     image.draw_data(tab, dir, str((max(numbers) if numbers != [] else 0) + 1))
 
