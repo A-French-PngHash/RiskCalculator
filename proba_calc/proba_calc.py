57e725a570f7691adef842b62bee115cb853007f
Titouan <75045140+PngHash-Titouan@users.noreply.github.com>
2024-10-29 21:37:25 +0100

    Add bombardier. WARNING : Doesn't work

--- proba_calc/proba_calc.py
+++ proba_calc/proba_calc.py
@@ -5,7 +5,7 @@ sys.path.append('../')
 
 from classes import Configuration
 
-config = Configuration([1, 0, 0],[0, 0, 0], True, 10, 6)
+config = Configuration([1, 0, 3],[0, 0, 0], False)
 computed_probabilities = {}
 
 class AttackResult:
@@ -56,13 +56,20 @@ def compute_battle(attack: int, defense:int, configuration : Configuration) -> f
 
     att_cap = configuration.vaisseaux_att[0]
     def_cap = configuration.vaisseaux_def[0]
+
+    att_chasseur = configuration.vaisseaux_att[2]
+    def_chasseur = configuration.vaisseaux_def[2]
     
 
-    defense_liste = [[i for i in range(1, 9 if (configuration.base or def_cap > dice) else 7)] for dice in range(def_pwr)]
-    attaque_liste = [[i for i in range(1, 9 if (att_cap > dice) else 7)] for dice in range(att_pwr)]
+    defense_liste = [[i for i in range(2 if (def_chasseur > dice) else 1 , 9 if (configuration.base or def_cap > dice) else 7)] for dice in range(def_pwr)]
+    attaque_liste = [[i for i in range(2 if (att_chasseur > dice) else 1, 9 if (att_cap > dice) else 7)] for dice in range(att_pwr)]
     
+
     def_dice = multiply_special(defense_liste)
     att_dice = multiply_special(attaque_liste)
+
+    print(att_dice)
+    print(def_dice)
 
 
     possibilities = len(def_dice) * len(att_dice)
@@ -74,11 +81,14 @@ def compute_battle(attack: int, defense:int, configuration : Configuration) -> f
         att_lose_one = 0
         for att in att_dice:
             for deff in def_dice:
-                if max(att) > max(deff):
+                max_att = max(att) + 1 if configuration.vaisseaux_att[1] >= 1 else 0
+                max_def = max(deff) + 1 if configuration.vaisseaux_def[1] >= 1 else 0
+
+                if max_att > max_def:
                     def_lose_one += 1
                 else:
                     att_lose_one += 1
-        proba = (def_lose_one * compute_battle(attack=attack, defense=defense - 1) + att_lose_one * compute_battle(attack=attack - 1, defense=defense))/possibilities
+        proba = (def_lose_one * compute_battle(attack=attack, defense=defense - 1, configuration=configuration) + att_lose_one * compute_battle(attack=attack - 1, defense=defense, configuration=configuration))/possibilities
        
     else:
         att_lose_two = 0
@@ -89,8 +99,23 @@ def compute_battle(attack: int, defense:int, configuration : Configuration) -> f
             for deff in def_dice:
                 satt = sorted(att, reverse=True)
                 sdeff = sorted(deff, reverse=True)
+
+                att_bomb = configuration.vaisseaux_att[1]
+                def_bomb = configuration.vaisseaux_def[1]
+
                 a1, a2 = satt[0], satt[1]
                 d1, d2 = sdeff[0], sdeff[1]
+
+                if att_bomb >= 1:
+                    a1 += 1
+                if att_bomb >= 2:
+                    a2 += 1
+                if def_bomb >= 1:
+                    d1 += 1
+                if def_bomb >= 2:
+                    d2 += 1
+
+
                 if a1 > d1 and a2 > d2:
                     def_lose_two +=1
                 elif a1 <= d1 and a2 <= d2:
@@ -98,14 +123,14 @@ def compute_battle(attack: int, defense:int, configuration : Configuration) -> f
                 else:
                     one_each += 1
 
-        proba = (att_lose_two * compute_battle(attack - 2, defense)
-                  + def_lose_two * compute_battle(attack, defense - 2)
-                  + one_each * compute_battle(attack - 1, defense - 1))/possibilities
+        proba = (att_lose_two * compute_battle(attack - 2, defense, configuration)
+                  + def_lose_two * compute_battle(attack, defense - 2, configuration)
+                  + one_each * compute_battle(attack - 1, defense - 1, configuration))/possibilities
         
         
     computed_probabilities[(attack, defense)] = proba
 
     return proba
 
-print(compute_battle(5, 1))
+print(compute_battle(5, 1, config))
 #print(computed_probabilities)
