1f5e5f4b5e909d3673ddbbc1885cbe509ec03d90
HomeyMura <186729939+HomeyMura@users.noreply.github.com>
2024-10-29 17:33:33 +0100

    cc

--- pilo/pilo.py
+++ pilo/pilo.py
@@ -11,9 +11,10 @@ vaisseaux?
 
 dirName= os.path.dirname(os.path.abspath(__file__))
 im = Image.new("RGB", (2000, 2000), "white")
-title_font = ImageFont.truetype(f"{dirName}/Fonts/Uni Sans Heavy.otf",
+title_font = ImageFont.truetype(f"{dirName}/Marianne/Marianne-ExtraBold.otf",
 size=30)
-body_font = ImageFont.truetype(f"{dirName}/Fonts/Uni Sans Thin.otf", size=20)
+body_font = ImageFont.truetype(f"{dirName}/Marianne/Marianne-Regular.otf", size=20)
+
 draw = ImageDraw.Draw(im)
 
 
