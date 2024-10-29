579fbaed9f95e9c98fa2dcf3f8663d6cba010c09
HomeyMura <186729939+HomeyMura@users.noreply.github.com>
2024-10-29 19:39:13 +0100

    rr

--- pilo/pilo.py
+++ pilo/pilo.py
@@ -4,31 +4,46 @@ from classes import tableau, Configuration
 
 
 dirName= os.path.dirname(os.path.abspath(__file__))
-im = Image.new("RGB", (20000, 20000), "white")
+config = Configuration([2,3,3],[2,3,3],True,50,50)
+tab = tableau(config,[["qejkfdgs","dsfds"],["dsfds","sfsf"]])
+im = Image.new("RGB", (1000*len(tab.liste_proba), 1000*len(tab.liste_proba)), "white")
 titleFont = ImageFont.truetype(f"{dirName}/Marianne/Marianne-ExtraBold.otf",
 size=30)
 generalFont = ImageFont.truetype(f"{dirName}/Marianne/Marianne-Regular.otf", size=20)
 draw = ImageDraw.Draw(im)
-config = Configuration([2,3,3],[2,3,3],True,50,50)
-tab = tableau(config,[["qejkfdgs","dsfds"],["dsfds","sfsf"]])
 
 draw.text((100, 100), f"Nombre de vaisseaux capitaux du joueur 1 : {str(config.vaisseaux_j1[0])}", font=generalFont, fill=(0, 0, 0))
-draw.text((100, 200), f"Nombre de vaisseaux capitaux du joueur 2 : {str(config.vaisseaux_j2[0])}", font=generalFont, fill=(0, 0, 0))
-draw.text((100, 300), f"Nombre de chasseurs du joueur 1 : {str(config.vaisseaux_j1[2])}", font=generalFont, fill=(0, 0, 0))
-draw.text((100, 400), f"Nombre de chasseurs du joueur 2 : {str(config.vaisseaux_j2[2])}", font=generalFont, fill=(0, 0, 0))
-draw.text((100, 500), f"Nombre de bombardiers du joueur 1 : {str(config.vaisseaux_j1[1])}", font=generalFont, fill=(0, 0, 0))
-draw.text((100, 600), f"Nombre de bombardiers du joueur 2 : {str(config.vaisseaux_j2[1])}", font=generalFont, fill=(0, 0, 0))
-draw.text((100, 700), f"Nombre de troupes du joueur 1 : {str(config.nb_troupes_j1)}", font=generalFont, fill=(0, 0, 0))
-draw.text((100, 800), f"Nombre de troupes du joueur 2 : {str(config.nb_troupes_j2)}", font=generalFont, fill=(0, 0, 0))
-draw.text((100, 900), f"Y a-t-il une base ? : {str(config.base)}", font=generalFont, fill=(0, 0, 0))
+draw.text((100, 150), f"Nombre de vaisseaux capitaux du joueur 2 : {str(config.vaisseaux_j2[0])}", font=generalFont, fill=(0, 0, 0))
+draw.text((100, 200), f"Nombre de chasseurs du joueur 1 : {str(config.vaisseaux_j1[2])}", font=generalFont, fill=(0, 0, 0))
+draw.text((100, 250), f"Nombre de chasseurs du joueur 2 : {str(config.vaisseaux_j2[2])}", font=generalFont, fill=(0, 0, 0))
+draw.text((100, 300), f"Nombre de bombardiers du joueur 1 : {str(config.vaisseaux_j1[1])}", font=generalFont, fill=(0, 0, 0))
+draw.text((100, 350), f"Nombre de bombardiers du joueur 2 : {str(config.vaisseaux_j2[1])}", font=generalFont, fill=(0, 0, 0))
+draw.text((100, 400), f"Nombre de troupes du joueur 1 : {str(config.nb_troupes_j1)}", font=generalFont, fill=(0, 0, 0))
+draw.text((100, 450), f"Nombre de troupes du joueur 2 : {str(config.nb_troupes_j2)}", font=generalFont, fill=(0, 0, 0))
+draw.text((100, 500), f"Y a-t-il une base ? : {str(config.base)}", font=generalFont, fill=(0, 0, 0))
 
 
 def drawTable(valueList):
     tablexpos = 100
-    tableypos = 1000
-    hauteurCellule = 200
-    largeurCellule = 200
+    tableypos = 550
+    hauteurCellule = 100
+    largeurCellule = 100
     margeInterieure = 10
+
+    #Dessine les fonds
+    for i in range(len(valueList)):
+        for j in range(len(valueList[0])):
+            x0 = tablexpos + j * largeurCellule
+            y0 = tableypos + i * hauteurCellule
+            x1 = x0 + largeurCellule
+            y1 = y0 + hauteurCellule
+            if 1>0.5:
+                couleur = (0,255,0)
+            elif 1<0.5:
+                couleur = (255,0,0)
+            else:
+                couleur = (255,165,0)
+            draw.rectangle([x0, y0, x1, y1], fill=couleur, outline=couleur)
 
     #Dessine les lignes
     for i in range(len(valueList)+1):
@@ -43,7 +58,7 @@ def drawTable(valueList):
         for j, column in enumerate(line):
             xpos = tablexpos + j * largeurCellule + margeInterieure
             ypos = tableypos + i * hauteurCellule + margeInterieure
-            draw.text((xpos, ypos), str(column), fill=(0, 0, 0), font=generalFont)
+            draw.text((xpos, ypos), str(column), fill=(0,0,0), font=generalFont)
 
 drawTable(tab.liste_proba)
 
