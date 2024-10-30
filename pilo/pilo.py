2d65d8d04212782d38fcaafee60b0130649bff0d
Your Name <you@example.com>
2024-10-30 01:08:18 +0100

    main

--- pilo/pilo.py
+++ pilo/pilo.py
@@ -125,4 +125,4 @@ tempConf = Configuration([2,3,3],[2,3,3],True)
 tempCase = Case(0.7,5)
 service = ImageDrawingService()
 cases =  [tempCase,tempCase,tempCase,tempCase,tempCase,tempCase,tempCase,tempCase,tempCase,tempCase,tempCase,tempCase,tempCase]
-service.draw_data(Tableau(tempConf,[cases, cases, cases]),dirName,"output")
+service.draw_data(Tableau(tempConf,[cases, cases, cases]),dirName,"output", False)
